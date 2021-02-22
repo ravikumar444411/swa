68c605d123e12cd73c64b764d0bf6b373c3842eb
ravikumar444411 <69462285+ravikumar444411@users.noreply.github.com>
2021-02-22 16:25:14 +0530

    base url change

--- src/views/Config.js
+++ src/views/Config.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://127.0.0.1:3001";
+const baseUrl = "https://swatest1.herokuapp.com";
 
 // get information of student start
 // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJhdmk0NDQ0MTEiLCJ1c2VyaWQiOiI2MDI4ZWZhZTkxOTQ4ZDM2MzhkODcwNjUiLCJpYXQiOjE2MTM0MTY0NjksImV4cCI6MTYxMzQyMDA2OX0.zSiqiMx6gwKnXBRSxecC8sH5K61xe4d0liyRZWqWckM';
